630b22a526ecd7de2d2f5155a5f6c2867e2c6e47
BenFletcher06 <benjamin.fletcher@uqconnect.edu.au>
2025-04-08 16:45:09 +1000

    Fixed low counter

--- low_counter.py
+++ low_counter.py
@@ -3,13 +3,13 @@
 def count_low(item_list):
     count = 0
     for item in item_list:
-        if item_list < 10:
+        if item < 10:
             count += 1
     return count
 
 # Testing
 test_list = [1, 3, 5, 12, 15, 8, 2]
 number_low = count_low(test_list)
-report = "Found " + count_low + " items less than 10."
+report = "Found " + str(number_low) + " items less than 10."
 
 print(report)
