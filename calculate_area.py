35286e3d288de1eca2e75fab2f2b4492d34437ac
yuvi175 <yuvrajkang627@gmail.com>
2025-04-08 16:37:54 +1000

    Fixed Calculated Area

--- calculate_area.py
+++ calculate_area.py
@@ -1,8 +1,8 @@
 # This script defines a function to calculate the area of a rectangle.
 def calculate_area(length, width):
-    area = lenght * width
+    area = length * width
     return area
 
 # Testing
 output = calculate_area(5, 10)
-print("The calculated area is:" output)
+print("The calculated area is:", output)
